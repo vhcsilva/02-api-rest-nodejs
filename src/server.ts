acd9fbcf18f05d186a5dc070d69c7009cf7df52e
vhcsilva <vhcsilvaa@gmail.com>
2024-05-05 22:04:04 -0300

    chore: configure production host

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running on port:', env.PORT)
